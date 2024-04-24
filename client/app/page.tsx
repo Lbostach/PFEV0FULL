6396a3f56ed9a839d6680d94945495546646e0e2
Lbostach <orxiedev@gmail.com>
2024-04-24 22:54:05 +0200

    All gets commited

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+
 
 export default function Home() {
 
@@ -7,7 +9,7 @@ return(
   <div className="grid min-h-screen">
       <header className="flex h-16 w-full shrink-0 items-center px-4 md:px-6 border-b">
         <Link className="flex items-center gap-2 text-lg font-semibold tracking-wider" href="#">
-          Ste
+          Logo
         </Link>
         <nav className="ml-auto flex items-center gap-4 text-sm font-medium md:gap-6">
           <Link
@@ -31,13 +33,20 @@ return(
         </nav>
       </header>
       <main className="flex-1">
-        <div className="container flex flex-col items-center justify-center py-16 text-center">
+        <div className="container flex flex-col items-center justify-center py-32 text-center">
           <h1 className="text-3xl font-bold leading-tight tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl/none">
             Bienvenue chez Ste
           </h1>
-          <p className="max-w-[600px] mt-3 text-gray-500 md:text-xl/relaxed dark:text-gray-400">
+          <p className="max-w-[600px] mt-3 text-gray-500 md:text-xl/relaxed">
             La solution marketing pour les petites et moyennes entreprises.
           </p>
+        </div>
+        <div className="container overflow-hidden h-[300px]">
+          <Image
+            src="/Teamwork.jpg"
+            width={1366}
+            height={800}
+          />
         </div>
       </main>
     </div>
